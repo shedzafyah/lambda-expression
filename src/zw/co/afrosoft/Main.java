1e704573f9d82cb9c1a5899c5e652ee439e2aa2c
shedzafyah <shedzafyah2@gmail.com>
2021-08-17 14:52:38 +0200

    Initial Commit

---
+++ src/zw/co/afrosoft/Main.java
@@ -0,0 +1,20 @@
+package zw.co.afrosoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //() -> System.out.println("lambda expression");
+
+        /*
+            addFunction=(int a, int b) -> a+b;
+
+            safeDivision=(int a, int b) -> {
+                if(b==0) return 0;
+            }
+
+            doubleFunction= (int a) -> a*2;
+
+         */
+    }
+}
