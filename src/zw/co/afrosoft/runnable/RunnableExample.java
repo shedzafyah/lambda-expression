1ef9e4728ebef8ef17a5c6720a73a87607b0f0c0
shedzafyah <shedzafyah2@gmail.com>
2021-08-17 16:05:56 +0200

    Demostrating the Runnable using lambda

---
+++ src/zw/co/afrosoft/runnable/RunnableExample.java
@@ -0,0 +1,14 @@
+package zw.co.afrosoft.runnable;
+
+public class RunnableExample {
+    public static void main(String[] args) {
+     Thread thread = new Thread(new Runnable() {
+         @Override
+         public void run() {
+             System.out.println("printed inside runnable");
+         }
+     });
+    }
+
+    Thread myLambdaThread = new Thread(()-> System.out.println("printed inside lambda runnable"));
+}
